5580952d444628fc88571f52b8759ba5b46c1c30
sbw01 <sbw01@outlook.dk>
2023-03-20 11:18:02 +0100

    Add project files.

---
+++ Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+   class Account
+    {
+        private double balance;
+
+       public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public double Withdraw(double amount)
+        {
+            if (balance > 0 && balance > amount) {
+                balance -= amount;
+                Console.WriteLine($"Remaining balance is {balance}");
+                return balance;
+            } else
+            {
+                Console.WriteLine("Balance is too low");
+                return balance;
+            }
+           
+            
+        }
+
+        public double Deposite(double amount)
+        {
+            
+            balance += amount;
+            return balance;
+        }
+
+
+        public void DisplayMessege()
+        {
+            Console.WriteLine("Your Balance {0}", Balance);
+        }
+       
+        public Account(double balance)
+        {
+            Balance = balance;
+        }
+
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main() {
+
+            
+
+            Console.WriteLine("Sparkassen Danmark, hvad kan vi gøre for dig");
+            Console.WriteLine("--------------------------------------------");
+            
+            Account obj = new Account(10000);
+
+            Console.WriteLine("1. Check Balance");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Deposit");
+            bool knownKeyPressed = false;
+           
+
+            do
+            {
+                ConsoleKeyInfo keyReaded = Console.ReadKey();
+                switch (keyReaded.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        obj.DisplayMessege();
+                        break;
+                    case ConsoleKey.D2:
+                        Console.Clear();
+                        Console.WriteLine("How much?");
+                        int amount = Convert.ToInt32(Console.ReadLine());
+                        obj.Withdraw(amount);
+                        obj.DisplayMessege();
+                        break;
+                    case ConsoleKey.D3:
+                        Console.Clear();
+                        Console.WriteLine("How much?");
+                        int amount2 = Convert.ToInt32(Console.ReadLine());
+                        obj.Deposite(amount2);
+                        obj.DisplayMessege();
+                        break;
+
+                }
+            } while (!knownKeyPressed);
+
+           
+
+
+        }
+    }
+}
